5a1b95e8202201807e72d0a3a4caa3645845c1c9
bobbybenz <sirbobbybenz@gmail.com>
2015-04-08 22:24:41 +0700

    Edit Symptom Node & Change value VIA ajax

--- SymptomNodeShow.php
+++ SymptomNodeShow.php
@@ -14,9 +14,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-            <form id="symptom-update" method="POST">
+            <form id="symptom-update" method="GET">
                 <h4>เลือกอาการ</h4>
-                <select id="symptom-list" data-placeholder="เลือกอาการ" class="selectpicker" data-live-search="true" name="symptomOption">
+                <select id="symptom-list" data-placeholder="เลือกอาการ" class="selectpicker" data-live-search="true" name="symptomID">
                       <option value="">       </option>
                 <?php while($objResultSymptom= mysql_fetch_array($objQuerysymptom)){?>
                       <option value="<?php echo $objResultSymptom['symptomID'];?>" 
@@ -40,24 +40,36 @@
         <div class="col-md-12">
     <?php
     if(isset($_GET['symptomID'])){
+       if($_GET['symptomID']!=""){
+       
+
         $strSQL = "SELECT * FROM symptomnode WHERE symptomID = ".$_GET['symptomID'];
-    $objQuery = mysql_query($strSQL) or die ("Error Query [".$strSQL."]");
-    $existData = 0;
-    //echo "Test :".$symptomID."<br>";
+        $objQuery = mysql_query($strSQL) or die ("Error Query [".$strSQL."]");
+
+        $existData = 0;
+        //echo "Test :".$objQuery."<br>";
     while($objResult= mysql_fetch_array($objQuery)){
         $existData = 1;
-        echo "ID = ".$objResult['symptomNodeID']."--";
+        //echo "<div id='display-node ".$objResult['symptomNodeID']."' data-toggle='modal' data-target='#testModal'>";
+        //echo "<div id='display-node' data-toggle='modal' data-target='#testModal'>";
+        //echo "<img src='test.jpeg' width='30' height='30' Onclick='Javascript:alert('imgTest');'/>";
+        ?>
+        <span  class="glyphicon glyphicon-edit" style="cursor: pointer;" Onclick="openModal('<?php echo $objResult['symptomNodeID'];?>')"></span>
+        <?php
+        echo "ID = ".$objResult['symptomNodeID']."---";
         echo $objResult['symptomID'];
         echo $objResult['isRoot'];
         echo $objResult['isYesNode'];
         echo $objResult['isNoNode'];
-        echo "<a data-toggle='modal' data-target='#testModal'>".$objResult['question']."</a>";
+        //echo "<span id='question-node ".$objResult['symptomNodeID']."' data-toggle='modal' data-target='#testModal'>".$objResult['question']."</span>";
+        echo "<span id='".$objResult['symptomNodeID']."'>".$objResult['question']."</span>"; 
         echo $objResult['yesNodeID'];
         echo $objResult['noNodeID'];
         echo $objResult['parentNodeID'];
         echo $objResult['haveAdditionData'];
         echo $objResult['typeAdditionData'];
-    
+        // echo "</div>";
+        // echo "<div style='float:left;'>";
         if($objResult['yesNodeID'] ==null){
             // echo "<input type='button' value='Yes Node' Onclick='JavaScript:alert(".$objResult['symptomNodeID'].")' >";  
         ?>
@@ -75,95 +87,10 @@
             Onclick='JavaScript:
             goToSymptomNodeAdd("noNode","<?php echo $objResult['symptomNodeID'];?>");
             '>
+
         <?php
         }//if($objResult['noNodeID'] ==null)
         
-        //if Last No of Symptom, I will be added disease.
-         if($objResult['yesNodeID'] ==null && $objResult['noNodeID'] ==null){
-            $strSQLCheck = "SELECT * FROM diseaseofsymptom WHERE symptomNodeID=".$objResult['symptomNodeID'];
-            $objQueryCheck = mysql_query($strSQLCheck);
-            $result = mysql_fetch_array($objQueryCheck);           
-            //echo "objQueryCheck:".$objQueryCheck."\n";
-            //echo "objQueryCheck: ".$result."\n";
-            if($result==null){
-                       
-    ?>
-        <a href = "DiseaseOfSymptomNodeManage.php?symptomNodeID=<?php echo $objResult['symptomNodeID'];?>&type=Add" > Add Disease </a>
-    
-    <?php
-        }//if($result==null)
-        else{
-        ?>
-            <a href = "DiseaseOfSymptomNodeManage.php?symptomNodeID=<?php echo $objResult['symptomNodeID'];?>&type=Add" > Edit Disease </a>
-        <?php }
-        }//if($objResult['yesNodeID'] ==null && $objResult['noNodeID'] ==null)
-        else{
-
-    ?>
-        <!-- <a href = "DiseaseOfSymptomNodeManage.php?symptomNodeID=<?php echo $objResult['symptomNodeID'];?>&type=View" > View Detail </a> -->
-    
-    <?php
-            }
-
-            echo "<br>";
-        }//while($objResult= mysql_fetch_array($objQuery))
-    if($existData==0){
-        ?>
-        <input type='button' value='Add Root Node' 
-            Onclick='JavaScript:
-            location.href = "SymptomNodeAddRoot.php?symptomID=<?php echo $symptomID;?>";
-            '>
-        <?php
-    }
-
-    }//Isset Get
-
-//
-    if(isset($_POST['symptomOption'])){
-        if($_POST['symptomOption'] !=""){
-    $strSQL = "SELECT * FROM symptomnode WHERE symptomID = ".$_POST['symptomOption'];
-    $objQuery = mysql_query($strSQL) or die ("Error Query [".$strSQL."]");
-    $existData = 0;
-    //echo "Test :".$symptomID."<br>";
-    while($objResult= mysql_fetch_array($objQuery)){
-        $existData = 1;
-        //echo "<div id='display-node ".$objResult['symptomNodeID']."' data-toggle='modal' data-target='#testModal'>";
-        //echo "<div id='display-node' data-toggle='modal' data-target='#testModal'>";
-    	echo "ID = <span id ='id-node'>".$objResult['symptomNodeID']."</span>---";
-    	echo $objResult['symptomID'];
-    	echo $objResult['isRoot'];
-    	echo $objResult['isYesNode'];
-    	echo $objResult['isNoNode'];
-    	//echo "<span id='question-node ".$objResult['symptomNodeID']."' data-toggle='modal' data-target='#testModal'>".$objResult['question']."</span>";
-        echo "<span id='question-node' data-toggle='modal' data-target='#testModal'>".$objResult['question']."</span>"; 
-    	echo $objResult['yesNodeID'];
-    	echo $objResult['noNodeID'];
-    	echo $objResult['parentNodeID'];
-    	echo $objResult['haveAdditionData'];
-    	echo $objResult['typeAdditionData'];
-        // echo "</div>";
-        // echo "<div style='float:left;'>";
-    	if($objResult['yesNodeID'] ==null){
-    		// echo "<input type='button' value='Yes Node' Onclick='JavaScript:alert(".$objResult['symptomNodeID'].")' >";	
-    	?>
-	    	<input type='button' value='Yes Node' 
-	    	Onclick='JavaScript:
-	    	goToSymptomNodeAdd("yesNode","<?php echo $objResult['symptomNodeID'];?>");
-	    	'>
-    	<?php
-    	}//if($objResult['yesNodeID'] ==null)
-
-    	if($objResult['noNodeID'] ==null){
-    	?>
-
-	    	<input type='button' value='No Node' 
-	    	Onclick='JavaScript:
-	    	goToSymptomNodeAdd("noNode","<?php echo $objResult['symptomNodeID'];?>");
-	    	'>
-
-    	<?php
-    	}//if($objResult['noNodeID'] ==null)
-    	
         //if Last No of Symptom, I will be added disease.
         if($objResult['yesNodeID'] ==null && $objResult['noNodeID'] ==null){
             $strSQLCheck = "SELECT * FROM diseaseofsymptom WHERE symptomNodeID=".$objResult['symptomNodeID'];
@@ -200,11 +127,9 @@
             location.href = "SymptomNodeAddRoot.php?symptomID=<?php echo $symptomID;?>";
             '>
         <?php
-    }
-
-
-    }//if($_POST['symptomOption'])!="")
-}//isset($_POST['symptomOption'])
+    }//END:if($existData==0)
+    }//END:$_GET['symptomID']!=""
+}//Isset Get
 else{//Donothing
     //echo "string";
 }
@@ -227,6 +152,7 @@ else{//Donothing
         <div class="form-group">
             <label>Question:</label>
             <input type="text" class="form-control" id="edit-question"></input>
+            <input type="hidden" id="hid-id"/>
         </div>
       </div>
       <div class="modal-footer">
@@ -243,6 +169,15 @@ else{//Donothing
    			window.location="SymptomNodeAdd.php?typeNode="+typeNode+"&symptomNodeID="+symptomNodeID;
    		}
 
+        function openModal(symptomNodeID){
+            var nodeid = symptomNodeID;
+            var nodequestion = document.getElementById(nodeid).innerHTML;
+            //alert('Onclick: '+nodequestion);
+            $('#testModal').modal('show');
+            $('#edit-question').val(nodequestion);
+            $('#hid-id').val(nodeid);
+        }
+
         //Query data when selector was changed 
         $(document).ready(function(){
             $('#symptom-list').change(function(){
@@ -250,16 +185,9 @@ else{//Donothing
                 //alert("test");
             });
 
-              $('#question-node').click(function(){
-                  var questionNode = $("#question-node").html();
-                  alert("test"+questionNode);
-                  $("#edit-question").val(questionNode);
-                  //$("#question-node").html("");              
-            });
-
             $('#button-update').click(function(){
                 var data = $('#edit-question').val();
-                var idNode = $('#id-node').html();
+                var idNode = $('#hid-id').val();
                 //alert("Data : "+data);
                  $.ajax({
                     type: "POST",
@@ -267,7 +195,7 @@ else{//Donothing
                     data: {data: data,type: "update",idnode:idNode},
                     success: function(result) {
                         //alert("result : "+result);
-                        $('#question-node').html(result);
+                        $('#'+idNode).html(result);
                         $('#testModal').modal('hide');
                     }
                 });
